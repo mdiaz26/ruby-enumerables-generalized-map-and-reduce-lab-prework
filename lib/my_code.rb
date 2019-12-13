544a017eaf542a0e99df32433350accee727b58b
Michael Diaz <mike.diaz006@gmail.com>
2019-12-13 16:33:22 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
 # Your Code Here
+def map(array)
+  ary = []
+  
+  array.each do |elem|
+  ary << yield(elem)
+end
+ary
+end
+
+def reduce(array, starting_point = nil)
+  if starting_point
+    sum = starting_point
+    i = 0
+  else
+    sum = array[0]
+    i = 1
+  end
+  while i < array.length
+  
+  sum = yield(sum, array[i])
+  i +=1
+  end
+sum
+end
