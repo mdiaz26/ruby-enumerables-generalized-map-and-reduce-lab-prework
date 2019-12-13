6cbcd90c47578b1bd399c1007b4ee5adcbec8412
Michael Diaz <mike.diaz006@gmail.com>
2019-12-13 16:21:26 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,7 +8,18 @@ end
 ary
 end
 
-def reduce(array, starting_point = 0)
-
-yield()
+def reduce(array, starting_point = nil)
+  if starting_point
+    sum = starting_point
+    i = 0
+  else
+    sum = array[0]
+    i = 1
+  end
+  while i > array.length
+  
+  sum = yield(sum, array[i])
+  i +=1
+  end
+sum
 end
